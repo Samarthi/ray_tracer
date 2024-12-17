dbde81665ad9b1aa8f1c15153b987a8451928e92
Samarthi Lahiri <Samarthi@solutionec.com>
2024-12-17 10:49:16 +0530

    added matrix inversion

--- lib/tuple.cpp
+++ lib/tuple.cpp
@@ -123,12 +123,62 @@ template <class T> struct Matrix {
 };
 
 int abs(Matrix<int> m) {
-  // TODO: find out if there exists an efficient approach to calculating
-  // determinants
-
-  return 0;
+  int result=0;
+  for(int i=0;i<m.width;i++){
+    if(m.length<=3){
+      int inner_det=(m.p_matrix[1][(j+1)%3]*m.p_matrix[2][(j+2)%3]-m.p_matrix[1][(j+2)%m]*m.p_matrix[2][(j+1)%3]);
+      result+=m.p_matrix[0][i]*inner_det;
+    }
+    else{
+      Matrix<int> inner_m={m.length-1,m.width-1,(int**)calloc(m.length-1,sizeof(int*))};
+      for(int j=0;j<m.length-1;j++)
+        inner_m.p_matrix[j]=(int)calloc(m.length-1,sizeof(int));
+      for(int j=1;j<=m.length-1;j++)
+        for(int k=1;k<=m.width-1;k++)
+          inner_m.p_matrix[j-1][i]=m.p_matrix[j][(j+k)%m.width];
+      result+=m.p_matrix[0][i]*abs(inner_m);   
+    }
+  }
+  return result;
 }
+Matrix<int> transpose(Matrix<int> m){
+  for(int i=0;i<m.length;i++)
+    for(int j=i+1;j<m.width;j++)
+      std::swap(m.p_matrix[i][j],m.p_matrix[j][i]);
+  return m;
+}
+
 Matrix<int> inverse(Matrix<int> m) {
-  // TODO: check if determinant is non-zero
+  int det=abs(m);
+  Matrix<int> cofactor={m.length,m.width,(int**)calloc(m.length,sizeof(int*))};
+  for(auto &p_c:cofactor)
+    p_c=(int)calloc(m.width,sizeof(int));
+  if(det){
+    for(int i=0;i<m.length;i++)
+      for(int j=0;j<m.width;j++){
+        Matrix<int> minor={m.length-1,m.width-1,(int**)calloc(m.length,sizeof(int*))};
+        for(auto &p_m:minor)
+          p_m=(int)calloc(m.width-1,sizeof(int));
+        //TODO: prove changing order of rows in matrix doesn't change determinant: (seems obvious tho)
+        //creating minor 
+        for(int k=1; k<=m.length-1; k++){
+          int col=0;
+          for(int l=0; l<m.width; l++)
+            if(l==j)
+              continue;
+            minor.p_matrix[k-1][col++]=m.p_matrix[(i+k)%m.length][l];
+        }
+        int min_det=abs(minor);
+        cofactor.p_matrix[i][j]=min_det;
+        if((i+j)&1)
+          cofactor.p_matrix[i][j]*=-1;
+      }
+    Matrix<int> adjugate=transpose(cofactor);
+    for(auto &row:adjugate)
+      for(auto &cell:row)
+        cell/=det;
+    return adjugate;    
+  }
+  else throw std::invalid_argument<"Matrix not invertible">;
   // find minors of each element
 }
