b74ba986ec14f87c2cdb2d44f325392b16ad5c73
Samarthi Lahiri <Samarthi@solutionec.com>
2025-11-24 18:57:23 +0530

    fix bugs in vec.h

--- include/vec.h
+++ include/vec.h
@@ -5,12 +5,12 @@
 template <typename T, size_t N>
 struct Vec{
 	T arr[N];
-	bool operator==(Vec<T,N> t1);
-	Tuple operator+(Vec<T,N> t1);
-	Tuple operator-(Vec<T,N> t1);
-	Tuple operator-();
-	Tuple operator*(T scalar);
-	Tuple operator/(T scalar);
-	Tuple operator*(Matrix<T, N, M> m);
-  	Tuple operator*(Vec<T,N> t);
-}
+	bool operator==(const Vec<T,N> &t1);
+	Vec<T,N> operator+(const Vec<T,N> &t1);
+	Vec<T,N> operator-(const Vec<T,N> &t1);
+	Vec<T,N> operator-();
+	Vec<T,N> operator*(T scalar);
+	Vec<T,N> operator/(T scalar);
+	Vec<T,N> operator*(const Matrix<T, N, M> &m);
+  	Vec<T,N> operator*(const Vec<T,N> &t);
+};
--- lib/vec.cpp
+++ lib/vec.cpp
@@ -1,105 +1,127 @@
 #pragma once
+#include "vec.h"
 #include "matrix.h"
+#include <cmath>
 #define EQ 0.0000008
+#include <iostream>
 
 template <typename T, size_t N>
-struct Vec{
-	alignas(32) T arr[N];
-
-	bool operator == (const Vec<T,N> &v){
-		for (int i = 0; i < N; ++i){
-
-			T abs_val = this->arr[i] - v.arr[i];
-			abs_val = abs_val > 0 ? abs_val : -1*abs_val;
-			if( abs_val > EQ)
-				return false;
-		}
-		return true;
+bool Vec<T, N>::operator==(const Vec<T, N> &v)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		T abs_val = this->arr[i] - v.arr[i];
+		abs_val = abs_val > 0 ? abs_val : -1 * abs_val;
+		if (abs_val > EQ)
+			return false;
 	}
+	return true;
+}
 
-	Vec<T,N> operator + (const Vec<T,N> &v){
-		Vec<T,N> result;
-		for(int i = 0;i < N; ++i)
-			result.arr[i] = this->arr[i]+v.arr[i];
-		return result;
+template <typename T, size_t N>
+Vec<T, N> Vec<T, N>::operator+(const Vec<T, N> &v)
+{
+	Vec<T, N> result;
+	for (int i = 0; i < N; ++i)
+		result.arr[i] = this->arr[i] + v.arr[i];
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> Vec<T, N>::operator-(const Vec<T, N> &v)
+{
+	Vec<T, N> result;
+	for (int i = 0; i < N; ++i)
+		result.arr[i] = this->arr[i] - v.arr[i];
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> Vec<T, N>::operator-()
+{
+	Vec<T,N> result;
+	for (int i = 0; i < N; ++i)
+		result.arr[i] = this->arr[i] *-1;
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> Vec<T, N>::operator*(T scalar)
+{
+	Vec<T, N> result;
+	for (int i = 0; i < N; ++i)
+	{
+		result.arr[i] = this->arr[i] * scalar;
 	}
-	
-	Vec<T,N> operator - (const Vec<T,N> &v){
-		Vec<T,N> result;
-		for(int i=0;i<N;++i)
-			result.arr[i] = this->arr[i] - v.arr[i];
-		return result;
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> Vec<T, N>::operator/(T scalar)
+{
+	Vec<T, N> result;
+	for (int i = 0; i < N; ++i)
+	{
+		result.arr[i] = this->arr[i] / scalar;
 	}
-	
-	Vec<T,N>& operator - (){
-		for(int i=0;i<N;++i)
-			this->arr[i]*=-1;
-		return *this;
+	return result;
+}
+
+template <typename T, size_t N, size_t M>
+Vec<T, M> Vec<T, N>::operator*(const Matrix<T, N, M> &m)
+{
+	Vec<T, M> result;
+	for (int i = 0; i < M; ++i)
+	{
+		result.arr[i] = 0;
+		for (int j = 1; j < N; ++j)
+			result.arr[i] += this->arr[i] * m.arr[i].arr[j];
 	}
-	
-	Vec<T,N> operator * (T scalar){
-		Vec<T,N> result;
-		for (int i = 0; i < N; ++i){
-			result.arr[i] = this->arr[i]*scalar;
-		}
-		return result;
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> cross(const Vec<T, N> &v1, const Vec<T, N> &v2)
+{
+	Vec<T, N> result;
+	for (int i = 0; i < N; i++)
+	{
+		result.arr[i] = v1.arr[(i + 1) % N] * v2.arr[(i + 2) % N] - v1.arr[(i + 2) % N] * v2.arr[(i + 1) % N];
 	}
-	
-	Vec<T,N> operator / (T scalar){
-		Vec<T,N> result;
-		for(int i=0;i<N;++i){
-			result.arr[i] = this->arr[i]/scalar;
-		}
-		return result;
+	return result;
+}
+
+template <typename T, size_t N>
+T dot(const Vec<T, N> &v1, const Vec<T, N> &v2)
+{
+	T result=0;
+	for (int i = 0; i < N; ++i)
+		result += v1.arr[i] * v2.arr[i];
+	return result;
+}
+
+template <typename T, size_t N>
+Vec<T, N> hadamard(const Vec<T, N> &v1, const Vec<T, N> &v2){
+	Vec<T, N> result;
+	for (int i = 0; i < N; ++i)
+		result.arr[i] = v1.arr[i] * v2.arr[i];
+	return result;
+}
+
+template <typename T, size_t N>
+void debug(const Vec<T, N> &v){
+	for (int i = 0; i < N; ++i){
+		std::cout << i << ' ' << v.arr[i] << ' ';
 	}
+	std::cout << endl;
+}
 
-	Vec<T,M> operator * (const Matrix<T, N, M> &m){
-		Vec<T,M> result;
-		for(int i=0;i<M;++i){
-			result.arr[i] = 0;
-			for(int j=0;j<N;++j)
-				result.arr[i] += this->arr[i]*m.arr[i].arr[j]; 
-		}
-		return result;
-	}	
-
-	Vec<T,M> cross(const Vec<T,N> &v1, const Vec<T,N> &v2){
-		Vec<T,M> result;
-		for(int i=0;i<N;i++){
-			result.arr[i] = v1.arr[(i+1)%N]*v2.arr[(i+2)%N] - v1.arr[(i+2)%N]*v2.arr[(i+1)%N];
-		}
+template <typename T, size_t N>
+T abs(const Vec<T, N> &v){
+	T result = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		result += v.arr[i] * v.arr[i];
 	}
-
-	T dot(const Vec<T,N> &v1, const Vec<T,N> &v2){
-		T result;
-		for(int i=0;i<N;++i)
-			result+=v1.arr[i]*v2.arr[i];
-		return result;
-	}
-
-	Vec<T,M> hadamard(const Vec<T,N> &v1, const Vec<T,N> &v2){
-		Vec<T,M> result;
-		for(int i=0;i<N;++i)
-			result.arr[i] = v1.arr[i]*v2.arr[i];
-		return result;
-	}
-
-	void debug(const Vec<T,N> &v){
-		for (int i = 0; i < N; ++i){
-			std::cout<<i<<' '<<v.arr[i]<<' ';
-		}
-		std::cout<<endl;
-	}
-
-	T abs(const Vec<T,N> &v){
-		T result=0;
-		for (int i = 0; i < N; ++i){
-			result+=v.arr[i]*v.arr[i];
-		}
-		return sqrt(result);
-	}
-
-};
-
-
-
+	return sqrt(result);
+}
