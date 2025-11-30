28527a968274318086ed92cefd87555537aae8b5
samarthi <samarthi.lahiri@gmail.com>
2025-11-30 16:35:09 +0530

    add Mat4 operator overloading and [] overloading for all primitives

--- include/vec.h
+++ include/vec.h
@@ -10,11 +10,17 @@ union Vec3 {
 	
 	float elements[3];
 	
-	//TODO: overload the [] operator 
+	inline float operator[](int idx) const{
+		return this->elements[idx];
+	}
+
+	inline float& operator[](int idx){
+		return this->elements[idx];
+	}
 	
 	inline bool operator==(const Vec3 &v) const{
 		for (int i = 0; i < 3; ++i){
-			float abs_val = this->elements[i] - v.elements[i];
+			float abs_val = (*this)[i] - v[i];
 			abs_val = abs_val > 0 ? abs_val : -1 * abs_val;
 			if (abs_val > EQ)
 				return false;
@@ -26,60 +32,60 @@ union Vec3 {
 	inline Vec3 operator+(const Vec3 &v) const{
 		Vec3 result;
 		for (int i = 0; i < 3; ++i)
-			result.elements[i] = this->elements[i] + v.elements[i];
+			result[i] = (*this)[i] + v[i];
 		return result;
 	}
 
 
 	inline Vec3& operator+=(const Vec3 &v){
 		for (int i = 0; i < 3; ++i)
-			this->elements[i] += v.elements[i];
+			(*this)[i] += v[i];
 		return *this;
 	}
 
 	inline Vec3 operator-(const Vec3 &v) const{
 		Vec3 result;
 		for (int i = 0; i < 3; ++i)
-			result.elements[i] = this->elements[i] - v.elements[i];
+			result[i] = (*this)[i] - v[i];
 		return result;
 	}
 
 	inline Vec3& operator-=(const Vec3 &v){
 		for (int i = 0; i < 3; ++i)
-			this->elements[i] -= v.elements[i];
+			(*this)[i] -= v[i];
 		return *this;
 	}
 
 	inline Vec3 operator-() const{
 		Vec3 result;
 		for (int i = 0; i < 3; ++i)
-			result.elements[i] = -1*this->elements[i];
+			result[i] = -1*(*this)[i];
 		return result;
 	}
 
 	inline Vec3 operator*(float scalar) const{
 		Vec3 result;
 		for (int i = 0; i < 3; ++i)
-			result.elements[i] = this->elements[i] * scalar;
+			result[i] = (*this)[i] * scalar;
 		return result;
 	}
 
 	inline Vec3& operator*=(float scalar){
 		for (int i = 0; i < 3; ++i)
-			this->elements[i] *= scalar;
+			(*this)[i] *= scalar;
 		return *this;
 	}
 
 	inline Vec3 operator/(float scalar) const{
 		Vec3 result;
 		for (int i = 0; i < 3; ++i)
-			result.elements[i] = this->elements[i] / scalar;
+			result[i] = (*this)[i] / scalar;
 		return result;
 	}
 
 	inline Vec3& operator/=(float scalar){
 		for (int i = 0; i < 3; ++i)
-			this->elements[i] /= scalar;
+			(*this)[i] /= scalar;
 		return *this;
 	}
 };
@@ -88,7 +94,7 @@ union Vec3 {
 inline Vec3 cross(const Vec3 &v1, const Vec3 &v2){
 	Vec3 result;
 	for (int i = 0; i < 3; i++)
-		result.elements[i] = v1.elements[(i + 1) % 3] * v2.elements[(i + 2) % 3] - v1.elements[(i + 2) % 3] * v2.elements[(i + 1) % 3];
+		result[i] = v1[(i + 1) % 3] * v2[(i + 2) % 3] - v1[(i + 2) % 3] * v2[(i + 1) % 3];
 	return result;
 }
 
@@ -96,27 +102,27 @@ inline Vec3 cross(const Vec3 &v1, const Vec3 &v2){
 inline float dot(const Vec3 &v1, const Vec3 &v2) {
 	float result=0;
 	for (int i = 0; i < 3; ++i)
-		result += v1.elements[i] * v2.elements[i];
+		result += v1[i] * v2[i];
 	return result;
 }
 
 inline Vec3 hadamard(const Vec3 &v1, const Vec3 &v2){
 	Vec3 result;
 	for (int i = 0; i < 3; ++i)
-		result.elements[i] = v1.elements[i] * v2.elements[i];
+		result[i] = v1[i] * v2[i];
 	return result;
 }
 
 inline void debug(const Vec3 &v) {
 	for (int i = 0; i < 3; ++i)
-		std::cout << i << ' ' << v.elements[i] << ' ';
+		std::cout << i << ' ' << v[i] << ' ';
 	std::cout << std::endl;
 }
 
 inline float abs(const Vec3 &v){
 	float result = 0;
 	for (int i = 0; i < 3; ++i)
-		result += v.elements[i] * v.elements[i];
+		result += v[i] * v[i];
 	return sqrt(result);
 }
 
@@ -124,7 +130,7 @@ inline Vec3 norm(const Vec3 &v){
 	float v_abs =abs(v);
 	Vec3 result;
 	for(int i=0;i<3;++i)
-		result.elements[i] = v.elements[i]/v_abs;
+		result[i] = v[i]/v_abs;
 	return result;
 }
 
@@ -135,7 +141,14 @@ union Vec4{
 	};
 	float elements[4];
 
-	//TODO: overload the [] operator 
+	inline float operator[](int idx) const{
+		return this->elements[idx];
+	}
+
+	inline float& operator[](int idx){
+		return this->elements[idx];
+	}
+	
 
 	inline bool operator==(const Vec4 &vec) const{
 		return this->v==vec.v && this->w == vec.w;
@@ -206,7 +219,7 @@ inline Vec4 hadamard(const Vec4 &v1, const Vec4 &v2){
 
 inline void debug(const Vec4 &v) {
 	for (int i = 0; i < 4; ++i)
-		std::cout << i << ' ' << v.elements[i] << ' ';
+		std::cout << i << ' ' << v[i] << ' ';
 	std::cout << std::endl;
 }
 
@@ -229,13 +242,50 @@ union Mat4{
 		Vec4 vectors[4]; //columns
 	};
 	float elements[16];
-	
-	//TODO: overload the [] operator 
 
-	bool operator== (Mat4 &m);
-	inline Mat4 operator+ (const Mat4 &m)const;
-	inline Mat4 operator- (const Mat4 &m)const;
-	inline Mat4 operator* (const Mat4 &m)const;
+	inline Vec4 operator[](int idx) const {
+		return this->vectors[idx];
+	}
+
+	inline Vec4& operator[](int idx){
+		return this->vectors[idx];
+	}
+
+	bool operator== (Mat4 &m)const {
+		for(int i=0;i<4;++i){
+			for(int j=0;j<4;++j)
+				if(not ((*this)[i][j]==m[i][j])) return false;	
+		}
+		return true;
+	}
+
+	inline Mat4 operator+ (const Mat4 &m)const{
+		Mat4 result = identity();
+		for(int i=0;i<4;++i)
+			for(int j=0;j<4;++j)
+				result[i][j] = (*this)[i][j] + m[i][j];
+		return result; 
+	}
+	
+	inline Mat4 operator- (const Mat4 &m)const{
+		Mat4 result = identity();
+		for(int i=0;i<4;++i)
+			for(int j=0;j<4;++j)
+				result[i][j] = (*this)[i][j] - m[i][j];
+		return result;
+	}
+	
+	inline Mat4 operator* (const Mat4 &m)const{
+		Mat4 result = identity();
+		for(int i=0;i<4;++i)
+			for(int j=0;j<4;++j){
+				result[i][j] = 0;
+				for(int k=0;k<4;++k)
+					result[i][j] += (*this)[i][k] - m[k][i];
+			}
+		return result;
+	}
+	
 	inline Mat4 operator- ()const{
 		//LU decomposition
 		Mat4 L = identity(), U=identity();
@@ -243,25 +293,54 @@ union Mat4{
 			for (int j = i; j < 4; ++j){
 				float subtract = 0.0;
 				for (int k = 0; k < i; ++k)
-					subtract+= L.vectors[i].elements[k] * U.vectors[k].elements[j]; 
-				U.vectors[i].elements[j] = this->vectors[i].elements[j] - subtract;
+					subtract+= L[i][k] * U[k][j]; 
+				U[i][j] = (*this)[i][j] - subtract;
 			}
-			for (int j = i+1; j < N; ++j){
-				subtract = 0.0;
+			for (int j = i+1; j < 4; ++j){
+				float subtract = 0.0;
 				for (int k = 0; k < i; ++k)
-					subtract+=L.vectors[j].elements[k] * U.vectors[k].elements[i];
-				L.vectors[j].elements[i] = (this->vectors[j].elements[i] - subtract)/U.vectors[i].elements[i];
+					subtract+=L[j][k] * U[k][i];
+				L[j][i] = ((*this)[j][i] - subtract)/U[i][i];
+			}
+		}		
+
+		//Y = L^-1, X = A^-1
+		
+		Mat4 X = identity(), Y = identity();
+		 
+		for(int i=0;i<4;++i){
+			for(int j=0;j<i;++j){
+				float subtract = 0.0;
+				for(int k=j;k<i;++k)
+					subtract+=Y[k][j]*L[i][k];
+				
+				Y[i][j] = -subtract;
 			}
 		}
-		
-		//TODO: substitution
-
-		return identity();
-
+	
+		for(int i=3;i>=0;--i){
+			for(int j=0;j<4;++j){
+				float subtract = 0.0;
+				for(int k=i+1;k<4;++k)
+					subtract += U[i][k]*X[k][j];
+				X[i][j] = (Y[i][j] - subtract)/U[i][i];
+			}
+		}
+		return X;
 	}
-	inline Mat4& operator+= (const Mat4 &m);
-	inline Mat4& operator-= (const Mat4 &m);
-	inline Mat4& operator*= (const Mat4 &m);	
+
+	inline Mat4& operator+= (const Mat4 &m){
+		for(int i=0;i<4;++i)
+			for(int j=0;j<4;++j)
+				(*this)[i][j] += m[i][j];
+		return (*this);
+	}
+	inline Mat4& operator-= (const Mat4 &m){
+		for(int i=0;i<4;++i)
+			for(int j=0;j<4;++j)
+				(*this)[i][j] -= m[i][j];
+		return (*this);
+	}	
 };
 
 
