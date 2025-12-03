a9b49e39be37a2b3328feb88ef373840c595e8cf
samarthi <samarthi.lahiri@gmail.com>
2025-12-04 01:32:24 +0530

    add rotation transforms

--- include/transforms.h
+++ include/transforms.h
@@ -1,4 +1,5 @@
 #include "Vec.h"
+#include <cmath>
 
 inline Vec4 point(float x, float y, float z){
 	Vec4 result  = {x,y,z,1};
@@ -27,8 +28,35 @@ inline Mat4 scale(float sx, float sy, float sz){
 }
 
 
-Mat4 rot_x(float theta);
-Mat4 rot_y(float theta);
-Mat4 rot_z(float theta);
+Mat4 rot_x(float theta){
+	Mat4 result = identity();
+	float cosine = cos(theta), sine = sin(theta);
+	result[1][1] = cosine;
+	result[2][1] = -sine;
+	result[1][2] = sine;
+	result[2][2] = cosine;
+	return result;
+}
+
+Mat4 rot_y(float theta){
+	Mat4 result = identity();
+	float cosine = cos(theta), sine = sin(theta);
+	result[0][0] = cosine;
+	result[2][0] = -sine;
+	result[0][2] = sine;
+	result[2][2] = cosine;
+	return result;
+}
+
+Mat4 rot_z(float theta){
+	Mat4 result = identity();
+	float cosine = cos(theta), sine = sin(theta);
+	result[0][0] = cosine;
+	result[1][0] = -sine;
+	result[0][1] = sine;
+	result[1][1] = cosine;
+	return result;
+}
+
 
 Mat4 look_at(const Vec4 &eye, const Vec4 &center, const Vec4 &up)
