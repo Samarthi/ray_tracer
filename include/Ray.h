3a471f85fca31b21e687d087aea3750dd37f67e3
samarthi <samarthi.lahiri@gmail.com>
2025-12-13 16:29:23 +0530

    add sphere intersection

--- include/Ray.h
+++ include/Ray.h
@@ -1,4 +1,5 @@
 #include "vec.h"
+#include "math.h"
 
 struct Ray {
   Vec4 origin, direction;
@@ -46,8 +47,42 @@ struct SphereIntersections{
 inline Vec4 ray_at(const Ray &r, float t){
   return r.origin + r.direction * t;
 }
+ 
+inline SphereIntersections intersect_sphere(const Ray &r, const Sphere &s){
+  SphereIntersections it;
 
-SphereIntersections intersect_sphere(const Ray &r, const Sphere &s);
-Intersection hit(const SphereIntersections &);
+  //uses implicit equation (O+tD-C)^2 - R^2 = 0
+
+  //D^2t^2 + 2(O-C)D*t + (O-C)^2-R^2 = 0
+  Vec4 oc = r.origin - s.origin;
+  float a = dot(r.direction, r.direction);
+  float b = 2*dot(r.origin - s.origin,r.direction);
+  float c = dot(oc, oc) - s.radius*s.radius; 
+  
+  //DISCRIMINANT  
+  float discriminant = b*b - 4*a*c;
+  
+  if (discriminant<0.0f){
+    it.hits = 0;
+    return it;
+  }
+  
+  float inv2a = 1.0f / (2.0f * a);
+  
+  if(float_eq(discriminant,0.0f)){
+    it.hits = 1;
+    it.hit1 = -b*inv2a;
+    return it;
+  }
+  
+  it.hits = 2;
+  it.hit1 = (-b - sqrt(discriminant))*inv2a;
+  it.hit2 = (-b + sqrt(discriminant))*inv2a;
+  return it;
+}
+
+Intersection hit(const SphereIntersections &s){
+
+}
 Vec4 normal_at(const Sphere &s, const Vec4 &p);
 Vec4 reflect(const Vec4 &incident, const Vec4 &normal);
